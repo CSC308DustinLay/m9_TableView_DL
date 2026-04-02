74634d0bb23bd970af4744774525ed878a865a10
Dustin <redhotblood1010@gmail.com>
2026-04-02 12:08:36 -0400

    yes

--- m9_TableView_DL/ViewController.swift
+++ m9_TableView_DL/ViewController.swift
@@ -7,9 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("#1", #function)
+        
+        //displays 100 cells
+        return 100
+    }
+    
+    //this method is called repeatedly until something
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("#1", #function, indexPath)
+        //#1 get or reuse a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        //#2 put data into cell
+        if indexPath.row < 19
+        {
+            cell.textLabel?.text = "\(indexPath.section) - \(indexPath.row)"
+        }
+        //#3 return cell
+        return cell
+    }
+    
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
